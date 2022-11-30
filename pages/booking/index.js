7baa09a89ac57b928707c98b5382ec6b255509b5
Karan Sable <karansable16@gmail.com>
2022-11-30 21:05:36 +0530

    TBD implementations

---
+++ pages/booking/index.js
@@ -0,0 +1,29 @@
+// 4 fields
+// area
+// maskCount
+// remdesivirCount
+// oxygencylinderCount
+// Sort suppliers in table in real time (when changed values in select field)
+
+//IMPORTANT: All the suppliers sell government authorised supplies with same rates.
+// a table with a button after each entry
+
+// the button opens up a modal where supplier details are displayed with a button to place an order
+
+// that button would open another model which would act as a checkout with a button to "Place Order"
+
+// that button would hit an api to reduce the count of supplies for the respective supplier
+
+//Once the "Order is placed", redirect to receipt page and amount to pay
+
+import React from 'react'
+
+const index = () => {
+  return (
+    <>
+      <div>Karan</div>
+    </>
+  )
+}
+
+export default index
